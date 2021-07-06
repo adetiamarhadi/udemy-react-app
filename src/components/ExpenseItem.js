dc8d70f5aafae98a800e085b14e178eead612b22
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-07-06 17:46:47 +0700

    dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2021, 6, 7);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 2100000
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">Rp{expenseAmount},00</div>
             </div>
         </div>
     );
