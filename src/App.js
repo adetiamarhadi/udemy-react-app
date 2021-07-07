3add3264ec25e50d5b18282e9e65957f0bc45c77
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-07-07 16:15:55 +0700

    assignment + composition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
 
@@ -12,44 +12,9 @@ function App() {
     { id: '6', title: 'Parkir - Mobil', amount: 33000, date: new Date(2021, 6, 4) },
     { id: '7', title: 'Ayam Geprek Bensu', amount: 27000, date: new Date(2021, 6, 4) }
   ];
-  
+
   return (
-    <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date} />
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date} />
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date} />
-
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date} />
-
-      <ExpenseItem
-        title={expenses[4].title}
-        amount={expenses[4].amount}
-        date={expenses[4].date} />
-
-      <ExpenseItem
-        title={expenses[5].title}
-        amount={expenses[5].amount}
-        date={expenses[5].date} />
-
-      <ExpenseItem
-        title={expenses[6].title}
-        amount={expenses[6].amount}
-        date={expenses[6].date} />
-    </div>
+    <Expenses expenses={expenses} />
   );
 }
 
---
+++ src/components/Expenses.js
@@ -0,0 +1,47 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+
+function Expenses(props) {
+
+    return (
+        <Card className="expenses">
+            <ExpenseItem
+                title={props.expenses[0].title}
+                amount={props.expenses[0].amount}
+                date={props.expenses[0].date} />
+
+            <ExpenseItem
+                title={props.expenses[1].title}
+                amount={props.expenses[1].amount}
+                date={props.expenses[1].date} />
+
+            <ExpenseItem
+                title={props.expenses[2].title}
+                amount={props.expenses[2].amount}
+                date={props.expenses[2].date} />
+
+            <ExpenseItem
+                title={props.expenses[3].title}
+                amount={props.expenses[3].amount}
+                date={props.expenses[3].date} />
+
+            <ExpenseItem
+                title={props.expenses[4].title}
+                amount={props.expenses[4].amount}
+                date={props.expenses[4].date} />
+
+            <ExpenseItem
+                title={props.expenses[5].title}
+                amount={props.expenses[5].amount}
+                date={props.expenses[5].date} />
+
+            <ExpenseItem
+                title={props.expenses[6].title}
+                amount={props.expenses[6].amount}
+                date={props.expenses[6].date} />
+        </Card>
+    );
+}
+
+export default Expenses;
