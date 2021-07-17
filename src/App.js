b81ce3eeb6325e83fdef35bfa77b89015ed8c312
Adetia Marhadi <adetia.marhadi@gmail.com>
2021-07-17 16:31:44 +0700

    adding form input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
@@ -14,7 +15,10 @@ const App = () => {
   ];
 
   return (
-    <Expenses expenses={expenses} />
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
   );
 }
 
